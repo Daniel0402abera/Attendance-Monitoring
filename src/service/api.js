8962d78c4fd0957db1a893a181e12779017d94e4
Daniel Abera <dsichale@gmail.com>
2024-01-20 23:43:10 +0300

    backend branch code

--- src/service/api.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://election-system-ecfw.onrender.com/',
-});
-
-export default instance;
